edfa70e859113825798639233aad295e12b36bd8
Natalie Spiska <natalie.spiska@gmail.com>
2024-09-26 06:25:32 -0400

    Added in the entire intro dialogue

--- src/Scripts/Level1/IntroScript.java
+++ src/Scripts/Level1/IntroScript.java
@@ -10,26 +10,26 @@ public class IntroScript extends Script {
         public ArrayList<ScriptAction> loadScriptActions() {
             ArrayList<ScriptAction> scriptActions = new ArrayList<>();
             scriptActions.add(new LockPlayerScriptAction());
-    
+
             scriptActions.add(new TextboxScriptAction() {{
                 addText("Park Ranger: Heading up the Giant, huh? ");
                 addText("Player: Beautiful day for some hiking, I think.");
                 addText("R: …Hiking? Say, the park closes in 3 hours. You\nwon't make it back in time, why don't\n you come back another day?");
-                addText("Player: *Laughs.* I've hiked harder trails faster than that. Thanks, but I'll be fine.  ");
-                addText("R: Listen, I'll be frank with you, boy. Strange things are happening on that mountain. It's not wise to head up the Giant now. ");
+                addText("Player: *Laughs.* I've hiked harder trails faster \nthan that. Thanks, but I'll be fine.  ");
+                addText("R: Listen, I'll be frank with you, boy. \nStrange things are happening on that mountain. \nIt's not wise to head up the Giant now. ");
                 addText("Player: Strange things? ");
-                addText("R: Rumors have been spreading by those who make it off the mountain… Grizzlies with glowing eyes, whispering trees attacking those who come too close… I've even seen some just like you go up and never come back.");
+                addText("R: Rumors have been spreading by those who make \nit off the mountain… Grizzlies with glowing eyes, whispering trees \nattacking those who come too close… I've even seen some \njust like you go up and never come back.");
                 addText("R: Some say the legend is to blame. ");
                 addText("Player: The legend? ");
                 addText("R: … ");
                 addText("R: The Legend of Hobbomock. ");
                 addText("Player: …Who?");
-                addText("R: That's precisely the point, boy. You're messing with things you don't understand. Stories that are coming to life… Spirits that are waking up. ");
-                addText("Player: Whatever man- people have made up crazier rumors on campus. I heard something about a pipe bomb last week?\t ");
-                addText("Player: Besides, I'm going on my hike, some random warnings won't stop me. ");
+                addText("R: That's precisely the point, boy. \nYou're messing with things you don't understand. \nStories that are coming to life… \nSpirits that are waking up. ");
+                addText("Player: Whatever man- people have made up\n crazier rumors on campus. I heard \nsomething about a pipe bomb last week?");
+                addText("Player: Besides, I'm going on my\n hike, some random warnings won't stop me. ");
                 addText("R: Say, what's your name boy? ");
-                addText("R: Ok ____ ... you seem like a nice kid. Take this, it could come in handy.  ");
-                addText("R: If you can keep your wits about you, go ahead and climb the Giant… ignore the warnings and the legends.  ");
+                addText("R: Ok ____ ... you seem like a nice \nkid. Take this, it could come in handy.  ");
+                addText("R: If you can keep your wits about \nyou, go ahead and climb the Giant… \nignore the warnings and the legends.  ");
                 addText("R: Just don't say I didn't warn you.");
             }});
     
