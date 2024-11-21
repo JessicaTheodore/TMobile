2de517bec28968e5bcc04ce2051cd8be601f2622
Your Name <jessicatheodore01@gmail.com>
2024-11-20 23:12:01 -0500

    jumpscare pic added

--- src/Maps/Level3ScreenMap.java
+++ src/Maps/Level3ScreenMap.java
@@ -15,7 +15,7 @@ import Utils.Colors;
 
         public Level3ScreenMap() {
             super("title_screen_map.txt", new CommonTileset());
-            back = new Sprite(ImageLoader.loadSubImage("temporaryBackground.png", Colors.MAGENTA, 0, 0, 800, 600));
+            back = new Sprite(ImageLoader.loadSubImage("Jumpscare.png", Colors.MAGENTA, 0, 0, 800, 600));
             back.setScale(1);
           //  back.setImageEffect(ImageEffect.FLIP_HORIZONTAL);
             back.setLocation(0, 0);
